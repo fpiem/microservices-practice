2bd20a71277df13452aac7fcc0b755bcb0c614de
Manuel Peli s291485 <s291485@studenti.polito.it>
2021-03-04 12:49:22 +0100

    Initial commit

---
+++ warehouse-service/src/main/kotlin/it/polito/ap/warehouseservice/WarehouseServiceApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.ap.warehouseservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarehouseServiceApplication>(*args)
+}
