ca9a4f945a73a639f916f1f3764304a81764cfab
Francesco Piemontese <s291491@studenti.polito.it>
2021-03-16 09:00:43 +0100

    remove unused comments

--- common/src/main/kotlin/it/polito/ap/common/dto/ProductDTO.kt
+++ common/src/main/kotlin/it/polito/ap/common/dto/ProductDTO.kt
@@ -4,19 +4,3 @@ data class ProductDTO (
     var productId: String,
     var price: Double
 )
-/*
-import java.lang.IllegalArgumentException
-
-class ProductDTO(
-    var name: String = "",
-    var description: String = "",
-    var picture: String = "",
-    var category: String = "", // TODO aggiungere CategoryType
-    var price: Double = 0.0
-) {
-    init {
-        if (price <= 0.0) {
-            throw IllegalArgumentException()
-        }
-    }
-}*/
