e48f019feaa3834afb80e8861e17040cd3053b52
Francesco Piemontese <s291491@studenti.polito.it>
2021-03-18 12:35:11 +0100

    clean WarehouseRepository

--- warehouse-service/src/main/kotlin/it/polito/ap/warehouseservice/repository/WarehouseRepository.kt
+++ warehouse-service/src/main/kotlin/it/polito/ap/warehouseservice/repository/WarehouseRepository.kt
@@ -1,18 +1,15 @@
 package it.polito.ap.warehouseservice.repository
 
 import it.polito.ap.warehouseservice.model.Warehouse
-import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface WarehouseRepository : MongoRepository<Warehouse, String> {
-    // TODO: check if String is fine or ObjectId is needed instead
+
     fun getWarehouseByWarehouseId(warehouseId: String): Warehouse?
 
-    // TODO aggiungete 'match' per controllare che la quantità del prodotto ('inventory.quantity') sia > 0
     @Aggregation(
         "{'\$unwind' : '\$inventory'}",
         "{'\$match' : { '\$and': [ {'inventory.productId': ?0}, {'inventory.quantity': {'\$gt': 0} } ] } }",
